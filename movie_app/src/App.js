004159bbd04201e690f622d97923c588e212a6c3
EunnyKoo <kooeun55555@gmail.com>
2023-12-28 16:21:07 +0900

    Clone Coding fin

--- movie_app/src/App.js
+++ movie_app/src/App.js
@@ -1,11 +1,22 @@
 import './App.css';
+import React from 'react';
+import { HashRouter, Route, Routes } from 'react-router-dom';
+import About from './routes/About';
+import Home from './routes/Home';
+import Navigation from './components/Navigation';
+import Detail from './routes/Detail';
 
 function App() {
   return (
-    <div >
-      <h1>hello</h1>
-    </div>
+    <HashRouter>
+      <Navigation />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/movie-detail" element={<Detail />} />
+      </Routes>
+    </HashRouter>
   );
 }
 
-export default App;  
+export default App;
---
+++ movie_app/src/routes/About.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './About.css';
+
+export default function About() {
+    return(
+        <div className='about__container'>
+            <span>About this page: I built it because I love movies</span>
+            <span>About this page: I built it because I love movies</span>
+        </div>
+    )
+}
